db57350df059f33171c080ffcc956a176d2e1e3b
Vladimir <vovagarmonshikov@mail.ru>
2023-10-31 11:08:34 +0300

    first commit 1

--- src/StringSchema.js
+++ src/StringSchema.js
@@ -1,12 +1,26 @@
-/* eslint-disable */
 export default class StringSchema {
+  constructor(validators) {
+    this.validators = [...validators];
+  }
 
-    constructor(validators) {
-        const defaultValid = [(value) => typeof(value) === 'string'];
-        this.validators = [...defaultValid, ...validators];
-    }
+  isValid(string) {
+    return this.validators.every((validator) => validator(string));
+  }
 
-    isValid(value) {
-        return this.validators.every((validator) => validator(value) === true);
-    }
+  startsFromUpperCase() {
+    const forbiddenValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '?', ' '];
+    const validator = (value) => (value.length > 0 ? value[0].toUpperCase() === value[0]
+        && !forbiddenValues.includes(value[0]) : false);
+    return new StringSchema([...this.validators, validator]);
+  }
+
+  length(num) {
+    const validator = (value) => value.length === num;
+    return new StringSchema([...this.validators, validator]);
+  }
+
+  hasExclamation() {
+    const validator = (value) => value.includes('!');
+    return new StringSchema([...this.validators, validator]);
+  }
 }
